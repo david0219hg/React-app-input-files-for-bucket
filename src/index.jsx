f65eb62410c2186c1c3ae607a1de9ce97b41e7b0
David Hurtado <david0219hg@gmail.com>
2023-04-21 13:03:55 -0500

    organizing errors

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-        <CssBaseline/>
         <App/>
-    </ThemeProvider>
   </React.StrictMode>
 );
